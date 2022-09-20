a8d1c37ffb3a58ebe634a619a58a7e7612dec88d
AlbertoLanchas <albertolsn7@gmail.com>
2022-09-20 10:55:37 +0200

    form submit and cards view
    Signed-off-by: AlbertoLanchas <albertolsn7@gmail.com>

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,32 +1,65 @@
-import React, { useState } from "react";
+import React from "react";
 import { images } from "../../constants";
 import { AppWrap, MotionWrap } from "../../wrapper";
-import logo from "../../assets/logo.png";
+import { useForm } from "../../hooks/useForm";
+import Message from "../../components/Message/Message";
+import Loader from "../../components/Loader/Loader";
 import "./Footer.scss";
 
-const Footer = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    message: "",
-  });
-  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const { username, email, message } = formData;
-  const handleChangeInput = (e) => {
-    const { username, value } = e.target;
-    setFormData({ ...formData, [username]: value });
-  };
+const initialForm = {
+  name: "",
+  email: "",
+  subject: "",
+  comments: "",
+};
 
-  const handleSubmit = () => {
-    setLoading(true);
-    const contact = {
-      _type: "contact",
-      username,
-      email,
-      message,
-    };
-  };
+const validationsForm = (form) => {
+  let errors = {};
+  let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+  let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+  let regexComments = /^.{1,255}$/;
+
+  if (!form.name.trim()) {
+    errors.name = "The field 'Name' is required";
+  } else if (!regexName.test(form.name.trim())) {
+    errors.name = "The field 'Name' leads only letters and spaces";
+  }
+
+  if (!form.email.trim()) {
+    errors.email = "The field 'Email' is required";
+  } else if (!regexEmail.test(form.email.trim())) {
+    errors.email = "The field 'Email' is wrong";
+  }
+
+  if (!form.subject.trim()) {
+    errors.subject = "The field 'Subject' is required";
+  }
+
+  if (!form.comments.trim()) {
+    errors.comments = "The field 'Comments' is required";
+  } else if (!regexComments.test(form.comments.trim())) {
+    errors.comments = "The field 'Comments' can't exceeded 255 characters";
+  }
+
+  return errors;
+};
+
+let styles = {
+  fontWeight: "bold",
+  color: "#dc3545",
+};
+
+const Footer = () => {
+  const {
+    form,
+    errors,
+    loading,
+    response,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+  } = useForm(initialForm, validationsForm);
+
   return (
     <>
       <h2 className="head-text">Get in Touch Today</h2>
@@ -45,49 +78,66 @@ const Footer = () => {
         </div>
       </div>
 
-      {!isFormSubmitted ? (
-        <div className="app__footer-form app__flex">
-          <div className="app__flex">
-            <input
-              type="text"
-              className="p-text"
-              placeholder="Your Name"
-              name="username"
-              value={username}
-              onChange={handleChangeInput}
-            />
-          </div>
-          <div className="app__flex">
-            <input
-              type="email"
-              className="p-text"
-              placeholder="Your Email"
-              name="email"
-              value={email}
-              onChange={handleChangeInput}
-            />
-          </div>
-          <div>
-            <textarea
-              className="p-text"
-              id=""
-              cols="30"
-              rows="10"
-              placeholder="Your Message"
-              name="message"
-              value={message}
-              onChange={handleChangeInput}
-            ></textarea>
-          </div>
-          <button type="button" className="p-text" onClick={handleSubmit}>
-            {loading ? "Sending" : "Send Message"}
-          </button>
+      <form className="app__flex app__footer-form " onSubmit={handleSubmit}>
+        <div className="app__flex">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your name"
+            className="p-text"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            value={form.name}
+            required
+          />
         </div>
-      ) : (
+        {errors.name && <p style={styles}>{errors.name}</p>}
+        <div className="app__flex">
+          <input
+            type="email"
+            name="email"
+            placeholder="Your email"
+            className="p-text"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            value={form.email}
+            required
+          />
+        </div>
+        {errors.email && <p style={styles}>{errors.email}</p>}
         <div>
-          <h3 className="head-text">Thank you for getting in touch!</h3>
+          <input
+            type="text"
+            name="subject"
+            placeholder="Mail subject"
+            className="p-text"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            value={form.subject}
+            required
+          />
         </div>
-      )}
+        {errors.subject && <p style={styles}>{errors.subject}</p>}
+        <div>
+          <textarea
+            name="comments"
+            cols="30"
+            rows="10"
+            placeholder="Your comments"
+            className="p-text"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            value={form.comments}
+            required
+          ></textarea>
+        </div>
+        {errors.comments && <p style={styles}>{errors.comments}</p>}
+        <button type="submit" value="Enviar" className="p-text">
+          SEND
+        </button>
+      </form>
+      {loading && <Loader />}
+      {response && <Message msg="The data has been send!" bgColor="#198754" />}
     </>
   );
 };
